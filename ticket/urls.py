c1ce196579ed2d3024744ad2567b39dd024e09cc
hossein.sayyedmousavi <hossein.sayyedmousavi@partsoftware.com>
2024-10-29 16:21:56 +0330

    update README.md file

--- ticket/urls.py
+++ ticket/urls.py
@@ -4,6 +4,6 @@ from . import views
 app_name = "ticket"
 
 urlpatterns = [
-    path('ticket/', views.TicketAPIView.as_view()),
-    path("ticket/<int:id>/",views.TicketDetailAPIView.as_view())
+    path('', views.TicketAPIView.as_view()),
+    path('<int:id>/',views.TicketDetailAPIView.as_view())
 ]
